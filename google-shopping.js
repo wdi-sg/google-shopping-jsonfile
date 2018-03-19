ed7737f2536b99c3b51a962df31a3699fa9cbe88
Ticktock <smu@ticktock.local>
2018-03-19 22:22:23 +0800

    Added assignment

--- google-shopping.js
+++ google-shopping.js
@@ -3,14 +3,33 @@ const jsonfile = require('jsonfile');
 
 const file = 'products.json'
 
+var question = function (obj) {
+	
+	var items = obj.items
+	var newList = []
+	// var brandCanonFromEbay = [];
+
+	for (var i = 0; i < items.length; i++) {
+		newList.push("Brand: " + items[i].product.brand);
+		newList.push("Price: " + items[i].product.inventories[0].price);
+		newList.push("Link: " + items[i].product.images[0].link)
+		}
+
+	return newList;
+	}
+
+	
+
+
 jsonfile.readFile(file, function(err, obj) {
-  console.dir(obj)
+	// console.dir(obj)
 
-  var resultFile = 'result.json'
-  var result = {name: 'JP'}
+	var resultFile = 'result.json';
+	
+	var result = question(obj); 
 
-  jsonfile.writeFile(resultFile, result, function (err) {
-    console.error(err)
-  });
+	jsonfile.writeFile(resultFile, result, function (err) {
+		console.error(err)
+	});
 
 });
