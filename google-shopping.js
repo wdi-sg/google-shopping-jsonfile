dbfdad02833971cabef750c897d0bb4a39272ecb
Alethea Toh <alethea.toh@duke.edu>
2018-03-19 16:44:42 +0800

    done

--- google-shopping.js
+++ google-shopping.js
@@ -8,12 +8,11 @@ jsonfile.readFile(file, function(err, obj) {
 
   var resultFile = 'result.json'
 
-  // Go through the items and find all results that have kind of shopping#product. Print the count of these results. Where else is this count information stored in the search results?
+`  Go through the items and find all results that have kind of shopping#product. Print the count of these results. Where else is this count information stored in the search results?`
   var items_filtered = obj.items.filter( item => item.kind === "shopping#product");
   var result = { itemsCount : items_filtered.length};
 
-  // Save the title of all items with a backorder availability in inventories.
-  // obj.items.forEach(function(x) {console.log(x.product.inventories);});
+`  Save the title of all items with a backorder availability in inventories.`
   var items_filtered = obj.items.filter( item => item.product.inventories[0].availability === "backorder");
   var result = { itemsBackorder : items_filtered};
 
@@ -24,28 +23,28 @@ jsonfile.readFile(file, function(err, obj) {
   result[process.argv[3]] = items_filtered;
   if (process.argv[2] === 'getkey') console.log(items_filtered);
 
-  // Save the title of all items with more than one image link.
-  // var items_filtered = obj.items.filter( item => item.product.images.length > 1);
-  // var result = { moreThanOne : items_filtered};
+`  Save the title of all items with more than one image link.`
+  var items_filtered = obj.items.filter( item => item.product.images.length > 1);
+  var result = { moreThanOne : items_filtered};
 
-  // Save all "Canon" products in the items (careful with case sensitivity).
-  // var items_filtered = obj.items.filter( item => item.product.brand.toLowerCase().includes('canon'));
-  // var result = { canon : items_filtered};
+`  Save all "Canon" products in the items (careful with case sensitivity).`
+  var items_filtered = obj.items.filter( item => item.product.brand.toLowerCase().includes('canon'));
+  var result = { canon : items_filtered};
 
-  // Save all items that have an author name of "eBay" and are brand "Canon".
-  // var items_filtered = obj.items.filter( item => item.product.brand.toLowerCase().includes('canon') && item.product.author.name.toLowerCase().includes('ebay'));
-  // var result = { canon_eBay : items_filtered};
+`  Save all items that have an author name of "eBay" and are brand "Canon".`
+  var items_filtered = obj.items.filter( item => item.product.brand.toLowerCase().includes('canon') && item.product.author.name.toLowerCase().includes('ebay'));
+  var result = { canon_eBay : items_filtered};
 
-  // Save all the products with their brand, price, and an image link
-  // var items_filtered = [];
-  // obj.items.forEach(function(item) {
-  //   newObj = {
-  //     brand: item.product.brand,
-  //     price: item.product.inventories[0].price,
-  //     imageLink: item.product.images[0].link,
-  //   };
-  //   items_filtered.push(newObj);
-  // });
+`  Save all the products with their brand, price, and an image link`
+  var items_filtered = [];
+  obj.items.forEach(function(item) {
+    newObj = {
+      brand: item.product.brand,
+      price: item.product.inventories[0].price,
+      imageLink: item.product.images[0].link,
+    };
+    items_filtered.push(newObj);
+  });
 
   jsonfile.writeFile(resultFile, result, function (err) {
     console.error(err)
