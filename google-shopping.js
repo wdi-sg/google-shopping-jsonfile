3861a904a7135be8792f5feba5e3addf096edff9
adna <adna@adna.com>
2018-06-20 17:28:32 +0800

    Complete Q1

--- google-shopping.js
+++ google-shopping.js
@@ -1,16 +1,32 @@
 // Write your solutions below
-const jsonfile = require('jsonfile');
+const myjsonfile = require('jsonfile');
+const answers = require("./previous-answers.js");
 
-const file = 'products.json'
+const file = 'products.json';
 
-jsonfile.readFile(file, function(err, obj) {
-  console.dir(obj)
+myjsonfile.readFile(file, function(err, obj) {
 
-  var resultFile = 'result.json'
-  var result = {name: 'JP'}
+  qOne(obj);
 
-  jsonfile.writeFile(resultFile, result, function (err) {
-    console.error(err)
+  var resultFile = 'result.json';
+  var result = { name: 'JP' }
+
+  myjsonfile.writeFile(resultFile, result, function(err) {
+    // console.error(err)
   });
 
 });
+
+
+
+let qOne = (products) => {
+  let counter = 0;
+  let items = products.items;
+
+  for (var i in items) {
+    if (items[i].kind === "shopping#product") {
+      counter += 1;
+    }
+  }
+  console.log(counter);
+}
