b42056e0ad3d31bd6fed7b52194ec6b780688baf
hygro5d <faidhiamran@Faidhis-MacBook-Pro.local>
2018-06-22 09:30:53 +0800

    first

--- google-shopping.js
+++ google-shopping.js
@@ -8,9 +8,51 @@ jsonfile.readFile(file, function(err, obj) {
 
   var resultFile = 'result.json'
   var result = {name: 'JP'}
+  var counter = 0;
 
-  jsonfile.writeFile(resultFile, result, function (err) {
-    console.error(err)
-  });
+  	for(var i=0; i<obj.items.length; i++) {
+  		if(obj.items[i].kind === “shopping#product”) {
+        itemCounter++;
+  }
+}
 
-});
+result['shoppingProductCount'].push(itemCounter);
+
+//PART 2
+
+result['backOrderAvailability'] = [];
+
+for (let i = 0; i < obj.items.length; i++) {
+  if (obj.items[i].product.inventories[0].availability == "backorder") {
+    result['backOrderAvailability'].push(obj.items[i].product.title);
+  }
+
+}
+
+
+//PART 3
+result['moreThanOneImg'] = [];
+
+for (let i = 0; i < obj.items.length; i++) {
+  if (obj.items[i].product.images.length>1){
+    result['moreThanOneImg'].push(obj.items[i].product.title);
+  }
+}
+
+//PART 4
+result['itemsWithCanon'] = [];
+
+for (let i = 0; i < obj.items.length; i++) {
+  if (obj.items[i].product.brand.toLowerCase() == "canon"){
+    result['itemsWithCanon'].push(obj.items[i].product.title);
+  }
+}
+
+//PART 5
+result['canonAndEbay'] = [];
+
+for (let i = 0; i < obj.items.length; i++) {
+  if (obj.items[i].product.title.search("Canon") >= 0  && obj.items[i].product.author.name.search("eBay") >= 0) {
+    result['canonAndEbay'].push(obj.items[i].product.title);
+  }
+}
