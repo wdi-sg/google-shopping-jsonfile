2bc2e784d4982a60a91703f1dbe0b214210a0bfb
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-21 22:10:43 +0800

    question 2 added

--- google-shopping.js
+++ google-shopping.js
@@ -10,16 +10,28 @@ jsonfile.readFile(file, function(err, obj) {
   var result = {};
   var item = obj.items;
 
-  function question1() {
-    var count = 0;
-    for(var i = 0; i < item.length; i++) {
-      if(item[i]['kind'] == 'shopping#product') {
-        count += 1;
-      };
-    };
-    result['shoppingProductCount'] = count.toString();   // convert answer to string and add to resultsObject
-  };
-  question1();
+  // function question1() {
+  //   var count = 0;
+  //   for(var i = 0; i < item.length; i++) {
+  //     if(item[i]['kind'] == 'shopping#product') {
+  //       count += 1;
+  //     };
+  //   };
+  //   result['shoppingProductCount'] = count.toString();   // convert answer to string and add to resultsObject
+  // };
+  // question1();
+
+  //Qn2
+  // var titles = [];
+  // function question2(){
+  //   for(var i = 0; i < item.length; i++) {
+  //     if (item[i]['product']['inventories'][0]['availability'] === "backorder"){
+  //       titles.push(item[i]['product']['title']);
+  //     }
+  //   };
+  //   result['titleBackorderInventories'] = titles;   // convert answer to string and add to resultsObject
+  // };
+  // question2();
 
 
   jsonfile.writeFile(resultFile, result, function (err) {
