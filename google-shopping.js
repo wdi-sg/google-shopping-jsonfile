148d4753d365a2c21c1dc710408a900ff651b941
ah5ive <ah5ive@gmail.com>
2018-09-21 17:29:25 +0800

    question 1

--- google-shopping.js
+++ google-shopping.js
@@ -1,16 +1,36 @@
 // Write your solutions below
-const jsonfile = require('jsonfile');
+// var count = 0;
 
-const file = 'products.json'
+var jsonfile = require('jsonfile');
+
+var file = 'products.json'
 
 jsonfile.readFile(file, function(err, obj) {
-  console.dir(obj)
 
-  var resultFile = 'result.json'
-  var result = {name: 'JP'}
+
+    var count = 0;
+    var items = obj.items;
+
+    for (var index in items) {
+      if (items[index].kind === "shopping#product") {
+        count += 1;
+      }
+    }
+
+  //console.dir(obj)
+
+  var resultFile = 'results.json'
+  var result = {}
+  result["count"] = count
 
   jsonfile.writeFile(resultFile, result, function (err) {
     console.error(err)
   });
 
 });
+
+
+
+
+
+
