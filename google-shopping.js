4c45c54b277557677207f3ac4e83c52636b255b9
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-21 21:44:11 +0800

    question 1 added

--- google-shopping.js
+++ google-shopping.js
@@ -1,15 +1,29 @@
 // Write your solutions below
-const jsonfile = require('jsonfile');
+var jsonfile = require('jsonfile');
 
-const file = 'products.json'
+var file = 'products.json'
 
 jsonfile.readFile(file, function(err, obj) {
   console.dir(obj)
 
-  var resultFile = 'result.json'
-  var result = {name: 'JP'}
+  var resultFile = 'results.json';
+  var result = {};
+  var item = obj.items;
+
+  function question1() {
+    var count = 0;
+    for(var i = 0; i < item.length; i++) {
+      if(item[i]['kind'] == 'shopping#product') {
+        count += 1;
+      };
+    };
+    result['shoppingProductCount'] = count.toString();   // convert answer to string and add to resultsObject
+  };
+  question1();
+
 
   jsonfile.writeFile(resultFile, result, function (err) {
+
     console.error(err)
   });
 
