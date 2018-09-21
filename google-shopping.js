269a67b8b8212ba0ad6bc6096f9d1bb3626f4c47
chanleyou <chanleyougmail.com>
2018-09-21 18:06:34 +0800

    completed to further 1

--- google-shopping.js
+++ google-shopping.js
@@ -1,16 +1,49 @@
 // Write your solutions below
 const jsonfile = require('jsonfile');
-
-const file = 'products.json'
+const functions = require('./functions.js');
+const file = 'products.json';
 
 jsonfile.readFile(file, function(err, obj) {
-  console.dir(obj)
 
-  var resultFile = 'result.json'
-  var result = {name: 'JP'}
+  var resultFile = 'result.json';
+  var result = {};
+
+  var items = obj.items;
+
+  // Q1:
+  result["shoppingProducts"] = functions.countShoppingProducts(items);
+
+  // Q2:
+  result["backOrderItems"] = functions.getBackorderItems(items);
+
+  // Q3:
+  result["itemsWithMultipleImages"] = functions.getMultipleImages(items);
+
+  // Q4:
+  var allCanon = functions.getByBrand(items, "Canon")
+  result["allCanon"] = allCanon;
+
+  // Q5:
+  result["eBayCanon"] = functions.getByAuthor(allCanon, "eBay");
+
+  // Q6:
+  result["itemsList"] = functions.listItems(items);
+
+  // Extra 1:
+  if (process.argv[2]) {
+    if (process.argv[2] === "getkey") {
+      console.log(result[process.argv[3]]);
+    }
+  }
+
+  // Extra 2:
+
+  // Extra 3:
 
   jsonfile.writeFile(resultFile, result, function (err) {
-    console.error(err)
+    if (err) {
+      console.error(err)
+    }
   });
 
 });
