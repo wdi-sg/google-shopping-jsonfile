289066a9c95d469fc5d97d0f49e1a0556eed273d
saufm68 <saufm68@gmail.com>
2018-09-22 14:41:37 +0800

    Done with all parts

--- google-shopping.js
+++ google-shopping.js
@@ -1,16 +1,113 @@
+const prevSolutions = require('./previous-answers.js');
+
 // Write your solutions below
 const jsonfile = require('jsonfile');
 
-const file = 'products.json'
+const file = 'products.json';
+var resultFile = 'results.json';
 
-jsonfile.readFile(file, function(err, obj) {
-  console.dir(obj)
+// jsonfile.readFile(file, function(err, obj) {
+//   console.dir(obj)
 
-  var resultFile = 'result.json'
-  var result = {name: 'JP'}
+//   var result = {name: 'JP'}
+//   result["count"] = prevSolutions.question1();
+//   result["titleBackorderInventories"] = prevSolutions.question2();
+//   result["titleWithMoreThanOneImage"] = prevSolutions.question3();
+//   result["allCanonBrands"] = prevSolutions.question4();
+//   result["eBayWithBrandCanon"] = prevSolutions.question5();
+//   result["allProducts"] = prevSolutions.question6();
 
-  jsonfile.writeFile(resultFile, result, function (err) {
-    console.error(err)
+//   jsonfile.writeFile(resultFile, result, function (err) {
+//     console.error(err)
+//   });
+
+// });
+
+// jsonfile.readFile(resultFile, function(err, obj) {
+
+//   console.log(obj);
+
+// });
+
+if (process.argv.length > 2) {
+
+  if (process.argv[2] === "getkey") {
+
+    var keyName = process.argv[3];
+
+    jsonfile.readFile(resultFile, function(err, obj) {
+
+      if (keyName === undefined) {
+
+        console.log(obj);
+
+      } else {
+
+        console.log(obj[keyName]);
+
+      }
+
+    });
+
+  }
+}
+
+if (process.argv[2] !== "getkey" && process.argv[2] !== "insertgoogleshopping" && process.argv[2] !== "insertart") {
+
+  var fileToRead = process.argv[2];
+
+  if (fileToRead.includes(".json") === false ) {
+
+    fileToRead += ".json";
+  }
+
+  jsonfile.readFile(fileToRead, function(err, obj) {
+
+    console.log(obj);
+
   });
 
-});
+}
+
+if (process.argv[2] === "insertgoogleshopping") {
+
+  var accessFile = file;
+
+  jsonfile.readFile(accessFile, function(err, obj) {
+
+    obj["items"].push(JSON.parse(process.argv[3]));
+
+    jsonfile.writeFile(accessFile, obj, function(err) {
+
+      console.log(err);
+
+    });
+
+  });
+
+} else if (process.argv[2] === "insertart") {
+
+  accessFile = 'collection.json';
+
+  jsonfile.readFile(accessFile, function(err, obj) {
+
+    obj["artObjects"].push(JSON.parse(process.argv[3]));
+
+    jsonfile.writeFile(accessFile, obj, function(err) {
+
+      console.log(err);
+
+    });
+
+  });
+
+}
+
+
+
+
+
+
+
+
+
