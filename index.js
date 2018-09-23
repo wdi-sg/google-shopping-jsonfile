5b1807c92a9e97ed00061f8e447abfef5a30ce1f
manieshablakey <manieshab@gmail.com>
2018-09-23 22:03:00 +0800

    modified index.js file

--- index.js
+++ index.js
@@ -10,131 +10,133 @@ const results = 'results.json'
 //Setting a count variable to count items retrieved.
 var count = 0;
 
+//Create a new object to hold all results.
+var allSearchResults = {};
+
 //Part 1a: Go through and find results that have kind of shopping#product.
+// var shoppingResults = [];
 // var shoppingProduct = function(err, products){
+//   // var shoppingResults = [];
 //   for (i = 0; i < products.items.length; i++) {
 //     var item = products.items[i];
 //     var itemName = item.product.title;
 //     if (item.kind == "shopping#product") {
 //       count++;
-//       resultsOutput.push(itemName);
-//       console.log(resultsOutput);
+//       console.log(count);
+//       shoppingResults.push(itemName);
+//       console.log(shoppingResults);
 //     }
 //   }
-//   var resultsAdd = {"Items with kind of shoppingProduct: ": resultsOutput};
-//   foo.writeFile(results, resultsAdd, function(err){
+//   var shoppingKind = {"Items with kind of shoppingProduct: ": shoppingResults};
+//   foo.writeFile(results, shoppingKind, function(err){
 //     console.log(err)
 //   });
 // };
 
 // foo.readFile(products, shoppingProduct)
 
-
 //(Use thie format): Part 1a: Go through and find results that have kind of shopping#product.
+var shoppingResults = [];
 foo.readFile(products, (err, products) => {
   for (i = 0; i < products.items.length; i++) {
     var item = products.items[i];
     var itemName = item.product.title;
     if (item.kind == "shopping#product") {
-      count++;
-      resultsOutput.push(itemName);
-      console.log(resultsOutput);
+      shoppingResults.push(itemName);
     }
   }
-  var resultsAdd = {"Items with shopping product in kind: ": resultsOutput};
-  foo.writeFile(results, resultsAdd, (err) => {
+  console.log(shoppingResults);
+  var shoppingKind = {"shoppingProductKind": shoppingResults};
+  foo.writeFile(results, shoppingKind, (err) => {
   console.log(err);
   });
 })
 
+
 //Part 1b: Go through object and save the title of all items with a backorder availability in inventories.
+var backorder = [];
 foo.readFile(products, (err, products) => {
   for (i = 0; i < products.items.length; i++) {
     var item = products.items[i];
     var itemName = item.product.title;
     if (item.product.inventories[0].availability === "backorder") {
-      count++;
-      resultsOutput.push(itemName);
-      console.log(resultsOutput);
+      backorder.push(itemName);
     }
   }
-  var resultsAdd = {"Items with backorder availability:": resultsOutput};
-  foo.writeFile(results, resultsAdd, (err) => {
+  var backorderItems = {"backorderAvailability": backorder};
+  foo.writeFile(results, backorderItems, (err) => {
   console.log(err);
   });
 })
 
-//Part 1c: Save the title of all items with more than one image link.
+// Part 1c: Save the title of all items with more than one image link.
+var images = [];
 foo.readFile(products, (err, products) => {
   for (i = 0; i < products.items.length; i++) {
     var item = products.items[i];
     var itemName = item.product.title;
     if (item.product.images.length > 1) {
-      count++;
-      resultsOutput.push(itemName);
-      console.log(resultsOutput);
+      images.push(itemName);
     }
   }
-  var resultsAdd = {"Items with more than one image link: ": resultsOutput};
-  foo.writeFile(results, resultsAdd, (err) => {
+  var moreImages = {"imageLink": images};
+  foo.writeFile(results, moreImages, (err) => {
   console.log(err);
   });
 })
 
 //Part 1d: Save all "Canon" products in the items.
+var canonBrand = [];
 foo.readFile(products, (err, products) => {
   for (i = 0; i < products.items.length; i++) {
     var item = products.items[i];
     var itemName = item.product.title;
     var itemBrand = item.product.brand.toLowerCase();
     if (itemBrand === "canon") {
-    count++;
-    resultsOutput.push(itemName);
-    console.log(resultsOutput);
+    canonBrand.push(itemName);
     }
   }
-  var resultsAdd = {"Canon products: ": resultsOutput};
-  foo.writeFile(results, resultsAdd, (err) => {
+  var canonItems = {"productsCanon": canonBrand};
+  foo.writeFile(results, canonItems, (err) => {
   console.log(err);
   });
 })
 
-// Part 1e: Save all items with an author name of "eBay" and are brand "Canon".
+Part 1e: Save all items with an author name of "eBay" and are brand "Canon".
+var authorBrand = [];
 foo.readFile(products, (err, products) => {
-  var authorBrand = [];
   for (i = 0; i < products.items.length; i++) {
     var item = products.items[i];
     var itemName = item.product.title;
     var itemBrand = item.product.brand.toLowerCase();
     var authorName = item.product.author.name.toLowerCase();
     if (itemBrand === "canon" && authorName === "ebay") {
-    count++;
     authorBrand.push(itemName);
     }
   }
-  var canonEbay = {"Canon items with eBay author: ": authorBrand};
+  var canonEbay = {"productsCanonEbay": authorBrand};
   foo.writeFile(results, canonEbay, (err) => {
   console.log(err);
   });
 })
 
 //Part 1f: Save all items with their brand, price and an image link.
+var threeFactors = [];
 foo.readFile(products, (err, products) => {
-  var threeFactors = [];
   for (i = 0; i < products.items.length; i++) {
     var item = products.items[i];
     var itemName = item.product.title;
     var itemBrand = item.product.brand.toLowerCase();
     var price = item.product.inventories[0].price;
     var imageLink = item.product.images[0].link;
-    count++;
     threeFactors.push(itemName);
   }
-  var brandPriceImage = {"Items with brand,price and image link: ": threeFactors};
-  foo.writeFile(results, threeFactors, (err) => {
+  var brandPriceImage = {"brandPriceImage: ": threeFactors};
+  foo.writeFile(results, brandPriceImage, (err) => {
     console.log(err);
   })
 })
+
 
 
 
